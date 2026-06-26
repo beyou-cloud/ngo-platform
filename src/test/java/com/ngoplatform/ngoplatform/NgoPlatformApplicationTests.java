d8dd5864cc1dc636e57bc60f7f58fd4b3e82e836
assiduous15 <siyagholap2@gmail.com>
2026-06-26 12:48:24 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/ngoplatform/ngoplatform/NgoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngoplatform.ngoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
